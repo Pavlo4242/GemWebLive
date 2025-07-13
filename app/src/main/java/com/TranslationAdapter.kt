0a8853728796fdcff5022df2c2c1bbabc278e0af
Pavlo4242 <pavlostepanchuk15@gmail.com>
2025-07-13 22:39:24 +0800

    Update TranslationAdapter.kt

--- app/src/main/java/com/TranslationAdapter.kt
+++ app/src/main/java/com/TranslationAdapter.kt
@@ -4,7 +4,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.gemweblive.databinding.ItemTranslationBinding
-import com.gemweblive.databinding.ItemTranslationBinding
 
 class TranslationAdapter : RecyclerView.Adapter<TranslationAdapter.TranslationViewHolder>() {
 
