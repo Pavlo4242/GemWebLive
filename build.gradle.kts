ccc89526eb7c0ee24e4dcaf3aef55eaa2afeda6c
Pavlo4242 <pavlostepanchuk15@gmail.com>
2025-07-14 20:14:26 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,30 @@
-// Top of your build.gradle.kts
+// build.gradle.kts (root project)
+
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
+        // Keep the Android Gradle Plugin classpath here
         classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20") // Add this line
+        // REMOVE this line: classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     }
 }
-// settings.gradle.kts
 
-pluginManagement {
+// Ensure you have a 'plugins' block here if you apply any top-level plugins (e.g., JVM, version catalog)
+// plugins {
+//     // id("your.top.level.plugin") version "x.y.z"
+// }
+
+allprojects {
     repositories {
-        gradlePluginPortal() // For Gradle plugins themselves (like Kotlin Android)
-        google()             // For Android-specific plugins and dependencies
-        mavenCentral()       // General Maven repository
-    }
-    // Define versions for plugins used in app/build.gradle.kts
-    resolutionStrategy {
-        eachPlugin {
-            // Apply version to org.jetbrains.kotlin.android
-            if (requested.id.id == "org.jetbrains.kotlin.android") {
-                useVersion("1.9.0") // Replace with your desired Kotlin version
-            }
-            // Add other plugin IDs and their versions here if needed
-            // For example, for com.android.application:
-            // if (requested.id.id == "com.android.application") {
-            //     useVersion("8.1.0") // Or whatever AGP version you use
-            // }
-        }
+        google()
+        mavenCentral()
+        // Add other repositories your project needs for dependencies
     }
 }
 
+// Other project-level configurations go here (e.g., tasks, ext properties)
 rootProject.name = "GemWebLive"
 include(":app")
