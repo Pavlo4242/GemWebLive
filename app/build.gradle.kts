693809f1a53fb6f12aa1c894bf9e4bde5e8bcaa5
Pavlo4242 <pavlostepanchuk15@gmail.com>
2025-07-14 21:47:06 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation 'com.squareup.okhttp3:okhttp:4.12.0' 
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
     // Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
