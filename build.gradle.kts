9e269bbfc11e2fc728354557bad4b2e3fd80fdea
Pavlo4242 <pavlostepanchuk15@gmail.com>
2025-07-14 20:04:40 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,14 @@
+// Top of your build.gradle.kts
+buildscript {
+    repositories {
+        google()  // This is REQUIRED for Android plugins
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2") // Use stable version
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.11.0" apply false
-    // Other plugins...
+    id("com.android.application") version "8.2.2" apply false
 }
