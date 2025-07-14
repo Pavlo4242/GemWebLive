7ee6d28278d14a3cbdc5c8c3966fe00ef2085657
Pavlo4242 <pavlostepanchuk15@gmail.com>
2025-07-14 16:13:54 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,6 @@ pluginManagement {
 }
 
 plugins {
-    id("com.android.application") version "8.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application") version "8.11.0" apply false
+    // Other plugins...
 }
