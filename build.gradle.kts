63d6bb7f78c64b6e47c106f21571cb4ea2213456
Pavlo4242 <pavlostepanchuk15@gmail.com>
2025-07-14 20:07:55 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,29 @@ buildscript {
     }
 }
 
-plugins {
-    id("com.android.application") version "8.2.2" apply false
+// settings.gradle.kts
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal() // For Gradle plugins themselves (like Kotlin Android)
+        google()             // For Android-specific plugins and dependencies
+        mavenCentral()       // General Maven repository
+    }
+    // Define versions for plugins used in app/build.gradle.kts
+    resolutionStrategy {
+        eachPlugin {
+            // Apply version to org.jetbrains.kotlin.android
+            if (requested.id.id == "org.jetbrains.kotlin.android") {
+                useVersion("1.9.0") // Replace with your desired Kotlin version
+            }
+            // Add other plugin IDs and their versions here if needed
+            // For example, for com.android.application:
+            // if (requested.id.id == "com.android.application") {
+            //     useVersion("8.1.0") // Or whatever AGP version you use
+            // }
+        }
+    }
 }
+
+rootProject.name = "GemWebLive"
+include(":app")
