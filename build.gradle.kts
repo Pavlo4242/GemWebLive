84e76aa2fc2f41cf4abef3b5e5f4ad1bebb7d1ae
Pavlo4242 <pavlostepanchuk15@gmail.com>
2025-07-14 15:16:13 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+pluginManagement {
+    repositories {
+        google()  // Add Google's repository first :cite[3]:cite[7]
+        gradlePluginPortal()
+    }
+}
+
 plugins {
+
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
