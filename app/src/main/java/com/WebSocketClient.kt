29a0a10155db37a344560fdeb34fcda59db88de5
Pavlo4242 <pavlostepanchuk15@gmail.com>
2025-07-16 09:35:34 +0700

    Update WebSocketClient.kt

--- app/src/main/java/com/WebSocketClient.kt
+++ app/src/main/java/com/WebSocketClient.kt
@@ -145,6 +145,7 @@ class WebSocketClient(
         }
         val setupConfig = mutableMapOf<String, Any>(
             "model" to "models/$modelName",
+            "responseModalities" to listOf("AUDIO"),
             "systemInstruction" to mapOf("parts" to instructionParts),
             "inputAudioTranscription" to emptyMap<String, Any>(),
             "outputAudioTranscription" to emptyMap<String, Any>(),
